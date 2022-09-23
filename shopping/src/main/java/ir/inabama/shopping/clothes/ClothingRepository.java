85a0eabce266499961a41cbb1abea5243ae68681
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2022-09-23 15:07:19 +0330

    shop service

--- shopping/src/main/java/ir/inabama/shopping/clothes/ClothingRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.inabama.shopping.clothes;
-
-import ir.inabama.shopping.clothes.ClothingItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClothingRepository extends JpaRepository<ClothingItem, Long> {
-}
--- shopping/src/main/java/ir/inabama/shopping/clothes/SortType.java
+++
@@ -1,9 +0,0 @@
-package ir.inabama.shopping.clothes;
-
-public enum SortType {
-    BUY_COUNT,
-    VIEW_COUNT,
-    LATEST,
-    PRICE_LOW,
-    PRICE_HIGH;
-}
